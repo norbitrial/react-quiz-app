8911c150a1faa79d8f50d97b330152ad109c5089
unknown <norbitrial8@gmail.com>
2020-06-06 10:55:19 +0200

    Corrected unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import { Router } from "react-router";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import IQuizState from "./Interfaces/IQuizState";
+import { createBrowserHistory } from "history";
+
+const quizReducer: IQuizState = {};
+const mockStore = configureMockStore();
+const store = mockStore({ quizReducer });
+const history = createBrowserHistory();
 
 test("renders without crash", () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  );
 });
